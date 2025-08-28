94312f8d2eeb88da465063fc3d259e75881738c6
tienmatinh <zemiken1120@gmail.com>
2025-08-28 15:43:18 +0700

    fix mongodb

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,49 +1,36 @@
-// lib/mongodb.ts
-import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb"
 
-const uri =
-  "mongodb+srv://hocvienphs:kienhd1231%40@cluster0.8ca8jsd.mongodb.net/tutien?retryWrites=true&w=majority&ssl=true";
+let clientPromise: Promise<MongoClient> | null = null
 
-const options = {
-  ssl: true,
-  serverSelectionTimeoutMS: 30000,
-  socketTimeoutMS: 0,
-  connectTimeoutMS: 30000,
-  maxPoolSize: 1,
-  minPoolSize: 0,
-  maxIdleTimeMS: 10000,
-  waitQueueTimeoutMS: 10000,
-  heartbeatFrequencyMS: 10000,
-  retryWrites: true,
-  retryReads: true,
-};
+const uri = "mongodb+srv://hocvienphs:kienhd1231%40@cluster0.8ca8jsd.mongodb.net/tutien?retryWrites=true&w=majority"
+const options = {}
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+let client
 
-declare global {
-  // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
-// Development: giữ clientPromise global để HMR
 if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>
   }
-  clientPromise = global._mongoClientPromise;
+
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    globalWithMongo._mongoClientPromise = client.connect()
+  }
+  clientPromise = globalWithMongo._mongoClientPromise
 } else {
-  // Production: mỗi serverless instance có riêng client
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
 }
 
-export default clientPromise;
-
-// Optional helper function nếu bạn muốn kết nối và lấy DB
 export async function connectToDatabase() {
-  const client = await clientPromise;
-  const db = client.db("tutien");
-  return { client, db };
+  const client = await clientPromise!
+  const db = client.db("tutien")
+  return { client, db }
 }
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
