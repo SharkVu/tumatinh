6188a94cb60a36509d7f592198841820016eca0b
tienmatinh <zemiken1120@gmail.com>
2025-08-28 14:43:29 +0700

    Remove vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,29 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Thêm các config debug
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: [
+      'mongodb', 
+      'bcryptjs', 
+      'jsonwebtoken',
+      '@mongodb-js/zstd',
+      'kerberos',
+      'snappy',
+      'socks'
+    ]
+  },
+  // Webpack config để handle các dependencies
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'bufferutil': 'commonjs bufferutil',
+      })
+    }
+    return config
+  }
 }
 
 export default nextConfig
